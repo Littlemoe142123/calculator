068674f80bd3456cc44273e4c6e95febf28ff2a9
Littlemoe142123 <139031777+Littlemoe142123@users.noreply.github.com>
2023-07-09 21:02:32 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let firstNumber = document.getElementById("firstNum");
+let secondNumber = document.getElementById("secNum");
+
+let calculation = document.getElementById("calculateBtn");
+let result = document.getElementById("result");
+
+calculateBtn.addEventListener("click",calculate);
+
+
+function calculate(){
+    let answer;
+
+    let num1= firstNumber.value;
+    let num2= secondNumber.value;
+
+    if(operator.value==="+")
+        {
+     answer= parseFloat(num1) + parseFloat(num2);}
+    else if(operator.value==="-")
+        {
+            answer= parseFloat(num1) - parseFloat(num2);}
+    else if(operator.value==="*")
+    {
+        answer= parseFloat(num1) * parseFloat(num2);
+    }
+    else if(operator.value==="/")
+    {
+        answer= parseFloat(num1) / parseFloat(num2);
+    }
+    
+        result.innerText = answer;
+}
